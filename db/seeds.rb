74f42eb53466cd1539c00c9f0150ed8cddebc6e4
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-07-27 21:13:34 +1000

    connected rails api and react front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 
 TestUser.destroy_all
 
-TestUser.create(name: 'Ben', description: 'Hi my name is Ben I like programming')
+TestUser.create(name: 'Ben', description: 'Hi my name is Ben I like Programming')
+TestUser.create(name: 'Jasmine', description: 'Hi my name is Jasmine I like Building')
 
 puts "TestUsers count: #{TestUser.count}"
