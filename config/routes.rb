1743a558c68c9589a87db26aa5b64aced0732766
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-07-27 20:34:17 +1000

    Added 'api/v1' namespace + TestUser seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :test_users
+  namespace :api do
+    namespace :v1 do
+      resources :test_users
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "test_users#index"
 end
